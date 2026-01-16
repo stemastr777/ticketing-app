870244573469b999b0494571402c8c5b98359eb4
stemastr <stevenadi16@gmail.com>
2026-01-16 08:59:06 +0700

    until Module 11

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -19,38 +19,38 @@
             </a>
         </li>
 
-            <!-- Kategori item -->
-            <li class="">
-                <a href="#" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Kategori">
-                    <!-- icon Kategori -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h6v6H4zm10 0h6v6h-6zM4 14h6v6H4zm10 3a3 3 0 1 0 6 0a3 3 0 1 0-6 0" />
-                    </svg>
-                    <span class="is-drawer-close:hidden">Manajemen Kategori</span>
-                </a>
-            </li>
+        <!-- Kategori item -->
+        <li class="{{ request()->routeIs('admin.categories.*') ? 'bg-gray-200 rounded-lg' : '' }}">
+            <a href="{{ route('admin.categories.index') }}" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Kategori">
+                <!-- icon Kategori -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h6v6H4zm10 0h6v6h-6zM4 14h6v6H4zm10 3a3 3 0 1 0 6 0a3 3 0 1 0-6 0" />
+                </svg>
+                <span class="is-drawer-close:hidden">Manajemen Kategori</span>
+            </a>
+        </li>
             
-            <!-- Event item -->
-            <li class="">
-                <a href="#" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Event">
-                    <!-- icon Event -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5h14a2 2 0 0 1 2 2v3a2 2 0 0 0 0 4v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-3a2 2 0 0 0 0-4V7a2 2 0 0 1 2-2" />
-                    </svg>
-                    <span class="is-drawer-close:hidden">Manajemen Event</span>
-                </a>
-            </li>
-            
-            <!-- History item -->
-            <li class="">
-	            <a href="#" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="History">
-	                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-	                    <circle cx="12" cy="12" r="10"></circle>
-	                    <polyline points="12 6 12 12 16 14"></polyline>
-	                </svg>
-	                <span class="is-drawer-close:hidden">History Pembelian</span>
-	            </a>
-		        </li>
+        <!-- Event item -->
+        <li class="{{ request()->routeIs('admin.events.*') ? 'bg-gray-200 rounded-lg' : '' }}">
+            <a href="{{ route('admin.events.index') }}" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Event">
+                <!-- icon Event -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5h14a2 2 0 0 1 2 2v3a2 2 0 0 0 0 4v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-3a2 2 0 0 0 0-4V7a2 2 0 0 1 2-2" />
+                </svg>
+                <span class="is-drawer-close:hidden">Manajemen Event</span>
+            </a>
+        </li>
+        
+        <!-- History item -->
+        <li class="{{ request()->routeIs('admin.histories.*') ? 'bg-gray-200 rounded-lg' : '' }}">
+            <a href="{{ route('admin.histories.index') }}" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="History">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <circle cx="12" cy="12" r="10"></circle>
+                    <polyline points="12 6 12 12 16 14"></polyline>
+                </svg>
+                <span class="is-drawer-close:hidden">History Pembelian</span>
+            </a>
+        </li>
         </ul>
 
         <!-- logout -->
